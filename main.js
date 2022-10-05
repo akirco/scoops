d2b3cd9681ccd13061589a659d65ebccf4c276c4
akirco <1833708553@qq.com>
2022-10-05 16:59:05 +0800

    complete

--- main.js
+++ main.js
@@ -3,15 +3,18 @@ const { searchLocal, searchRemote } = require("./lib/utils");
 
 function search_local(args) {
   if (args.length !== 0) {
-    const app = args[0].startsWith("-") ? args[1] : args[0];
+    const app = args[0].startsWith("-") ? args[1].trim() : args[0].trim();
     searchLocal(app);
   }
 }
 
 async function search_remote(args) {
   if (args.length !== 0) {
-    const app = args[0].startsWith("-") ? args[1] : args[0];
-    searchRemote(app);
+    const app = args[0].startsWith("-") ? args[1].trim() : args[0].trim();
+    const count =
+      parseFloat(args[2]).toString() !== "NaN" ? parseFloat(args[2]) : 20;
+
+    searchRemote(app, count);
   }
 }
 
